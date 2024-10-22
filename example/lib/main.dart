b99060c4eb18d83a72107f7e1cd615a288af9190
Ernest Eferetin <ernesteferetin@yahoo.com>
2024-10-22 08:43:09 +0100

    Worked on the margin between flag and the country name and also ensured sound-null saftey is enabled

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -5,7 +5,7 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatefulWidget {
-  const MyApp({Key key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   MyAppState createState() => MyAppState();
@@ -107,10 +107,12 @@ class MyAppState extends State<MyApp> {
                 favorite: const ['+39', 'FR'],
                 countryFilter: const ['IT', 'FR'],
                 showFlagDialog: false,
-                comparator: (a, b) => b.name.compareTo(a.name),
+                //You can set the margin between the flag and the country name to your taste.
+                margin: const EdgeInsets.symmetric(horizontal: 6),
+                comparator: (a, b) => b.name!.compareTo(a.name!),
                 //Get the country information relevant to the initial selection
                 onInit: (code) => debugPrint(
-                    "on init ${code.name} ${code.dialCode} ${code.name}"),
+                    "on init ${code?.name} ${code?.dialCode} ${code?.name}"),
               ),
               CountryCodePicker(
                 onChanged: print,
